3e4e3522f17a240f3fb27802037d5ef9c59c67ce
Esef_Technolgy <eseftechnology@gmail.com>
2026-01-21 13:01:03 +0000

    Blog-Pages

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import GroupServices from './COMPONENTS/PAGES/SERVICES/Group-Services';
 import About from './COMPONENTS/PAGES/ABOUT/About';
 import Contact from './COMPONENTS/PAGES/CONTACT/Contact';
 import Careers from './COMPONENTS/PAGES/CAREERS/Careers';
+import Blog from './COMPONENTS/PAGES/BLOG/Blog';
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/careers" element={<Careers />} />
+          <Route path="/blog" element={<Blog />} />
         
       
       </Routes>
