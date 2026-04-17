090ef849650b688e96990145b552700fb9cdd3d9
esef-tech <eseftechnology.com>
2026-04-17 23:49:08 +0000

    update-navbar-login-button-ui

--- src/COMPONENTS/Navbar.jsx
+++ src/COMPONENTS/Navbar.jsx
@@ -90,10 +90,12 @@ const AppNavbar = () => {
                 </div>
               ) : (
                 <>
-                  <Button  className="me-2 login-btn" as={Link} to="/signin"> <FaUserCircle className="me-1" />
+                  <Button variant="outline-primary" className="me-2" as={Link} to="/signin" style={{ backgroundColor: '#17a2b8' }}>
+                    <FaUserCircle className="me-1" />
                     Sign In
                   </Button>
-                  <Button className="me-2 login-btn"  as={Link} to="/signup"> <FaUserCircle className="me-1" />
+                  <Button className="me-2 login-btn" as={Link} to="/signup">
+                    <FaUserCircle className="me-1" />
                     Sign Up
                   </Button>
                 </>
