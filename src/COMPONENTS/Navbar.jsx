16db16de366c8796ae262b6149063484500ebacb
esef-tech <eseftechnology.com>
2026-04-18 05:48:56 +0000

    update-the-code-base

--- src/COMPONENTS/Navbar.jsx
+++ src/COMPONENTS/Navbar.jsx
@@ -90,7 +90,7 @@ const AppNavbar = () => {
                 </div>
               ) : (
                 <>
-                  <Button variant="outline-primary" className="me-2" as={Link} to="/signin" style={{ backgroundColor: '#17a2b8' }}>
+                  <Button  className="me-2 text-light" as={Link} to="/signin" style={{ backgroundColor: '#17a2b8' }}>
                     <FaUserCircle className="me-1" />
                     Sign In
                   </Button>
